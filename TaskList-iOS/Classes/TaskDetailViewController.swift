94582b14236b4596c2bd103a8676349df768654c
xiongmaomaomao <pasapasa.va1104@gmail.com>
2016-07-03 22:36:30 +0900

    add empty TaskDetailView

---
+++ TaskList-iOS/Classes/TaskDetailViewController.swift
@@ -0,0 +1,13 @@
+//
+//  TaskDetailViewController.swift
+//  TaskList-iOS
+//
+//  Created by KAKEGAWA Atsushi on 2016/07/03.
+//  Copyright © 2016年 KAKEGAWA Atsushi. All rights reserved.
+//
+
+import UIKit
+
+class TaskDetailViewController: UIViewController {
+    
+}
