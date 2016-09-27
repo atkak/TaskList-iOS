67045804203511803a9863a0a080f0f10fecc2a7
xiongmaomaomao <pasapasa.va1104@gmail.com>
2016-09-27 13:00:27 +0900

    introduce rx into vc

--- TaskList-iOS/Classes/TaskListViewController.swift
+++ TaskList-iOS/Classes/TaskListViewController.swift
@@ -1,62 +1,96 @@
 import UIKit
 import BrightFutures
+import RxSwift
+import RxCocoa
 
-class TaskListViewController: UITableViewController {
+class TaskListViewController: UIViewController {
     
-    private var tasks: [Task]?
-    private let fetchTaskListService = FetchTaskListService()
+    private let presenter = TaskListPresenter()
+    private let disposeBag = DisposeBag()
+    
+    @IBOutlet var tableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        loadTasks()
-    }
-    
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return self.tasks?.count ?? 0
-    }
-    
-    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = self.tableView.dequeueReusableCellWithIdentifier("CellIdentifier", forIndexPath: indexPath)
-        if let task = self.tasks?[indexPath.row] {
-            cell.textLabel?.text = task.title
-            cell.accessoryType = task.completed ? .Checkmark : .None
-        }
+        self.presenter.taskStream
+            .drive(self.tableView.rx_itemsWithCellIdentifier("CellIdentifier")) { _, task, cell in
+                cell.textLabel?.text = task.title
+                cell.accessoryType = task.completed ? .Checkmark : .None
+            }
+            .addDisposableTo(disposeBag)
         
-        return cell
+        self.presenter.alertEventStream
+            .driveNext { alertEvent in
+                let alert = UIAlertController(title: alertEvent.title, message: alertEvent.message, preferredStyle: .Alert)
+                alert.addAction(UIAlertAction(title: "OK", style: .Default, handler: nil))
+                self.presentViewController(alert, animated: true, completion: nil)
+            }
+            .addDisposableTo(disposeBag)
+        
+        self.tableView.rx_itemSelected
+            .map { $0.row }
+            .bindTo(presenter.onSelected)
+            .addDisposableTo(disposeBag)
+        
+        self.presenter.loadTasks()
     }
     
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
         if let identifier = segue.identifier where identifier == "PushToDetailForEdit" {
             let destinationViewController = segue.destinationViewController as! TaskDetailViewController
-            let index = self.tableView.indexPathForSelectedRow!.row
-            destinationViewController.task = self.tasks![index]
+            destinationViewController.task = self.presenter.selectedTask
         }
     }
     
     @IBAction func unwindFromDetailViewFor(segue: UIStoryboardSegue) {
-        loadTasks()
+        self.presenter.loadTasks()
     }
     
-    private func loadTasks() {
-        let future = fetchTaskListService.findAll()
-        future
-            .onSuccess(Queue.main.context) {
-                self.tasks = $0
-                self.tableView.reloadData()
-            }
-            .onFailure(Queue.main.context) { error in
-                let alert = UIAlertController(title: nil, message: "Fail to load tasks", preferredStyle: .Alert)
-                alert.addAction(UIAlertAction(title: "OK", style: .Default, handler: nil))
-                self.presentViewController(alert, animated: true, completion: nil)
-            }
-    }
-
 }
 
+class TaskListPresenter {
+    private let fetchTaskListService = FetchTaskListService()
+    
+    private let tasks = Variable<[Task]>([])
+    
+    private let alertEvent = Variable<AlertEvent!>(nil)
+    
+    private (set) var selectedTask: Task? = nil
+    
+    init() {
+        taskStream = tasks.asDriver()
+        alertEventStream = alertEvent.asDriver().filter { $0 != nil }
+    }
+    
+    let taskStream: Driver<[Task]>
+    
+    let alertEventStream: Driver<AlertEvent!>
+    
+    private func bindSelectedTask(event: Event<Int>) {
+        if case let .Next(index) = event {
+            selectedTask = tasks.value[index]
+        }
+    }
+    
+    var onSelected: AnyObserver<Int> {
+        return AnyObserver(eventHandler: bindSelectedTask)
+    }
+    
+    func loadTasks() {
+        let future = fetchTaskListService.findAll()
+        future
+            .onSuccess(Queue.main.context) { [unowned self] in
+                self.tasks.value = $0
+            }
+            .onFailure(Queue.main.context) { [unowned self] _ in
+                self.alertEvent.value = AlertEvent(message: "Fail to load tasks")
+            }
+    }
+}
+
+struct AlertEvent {
+    let title: String? = nil
+    let message: String
+}
 
